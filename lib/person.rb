8077464a1da091ea7b4bd5864d28960830b6b0e8
Bridgett Wheeler <bridgettwheeler@gmail.com>
2022-02-10 13:18:48 -0500

    getting help

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,21 @@
-# your code goes here
+class Person 
+    attr_reader :name
+    attr_accessor :bank_account, :happiness, :hygiene
+
+    def initialize(name)
+        @name = name
+      end
+     
+     def initialize(bank_account)
+        @bank_account = $25
+     end 
+
+     def initialize(happiness)
+        @happiness = 8
+     end
+
+     def initialize(hygiene)
+        @hygiene = 8
+     end
+
+end
